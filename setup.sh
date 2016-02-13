6b45c5ae902247a36152e265ba3706d8c44de4fb
DaniHaag <dhaag@web.de>
2016-02-13 08:07:20 +0000

    added apt-get dist-upgrade

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-sudo apt-get install maven
+#sudo apt-get update && sudo apt-get dist-upgrade
+git config --global user.email "dhaag@web.de"
+git config --global user.name "DaniHaag"
+#sudo apt-get install maven
